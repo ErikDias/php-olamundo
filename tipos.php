8c718276f9e96461822b031222ea305e613b51ee
Erik Dias <erikdias@yahoo.com.br>
2022-01-12 00:17:55 -0300

    Update tipos.php

--- tipos.php
+++ tipos.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP tipos</title>
+    <title>Olá Mundo Heroku/title>
 </head>
 <body>
     <?php
-    $n=4;
-    $no="Gustavo";
-    echo $no;
+    echo "Ola Mundo";
     ?>
 </body>
 </html>
