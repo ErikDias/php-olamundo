d3580626f341ab2daa5bbecffd4559b642358c89
Erik Dias <erikdias@yahoo.com.br>
2022-01-29 23:17:47 -0300

    novo commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include 'conexao.class.php'
 
 #instanciando o objeto
-$conexao = new Conexao('$this->banco');
+$conexao = new Conexao('dd8pp99pbdat4j');
 
 #chamada ao metodo open que abra a conexao
 $conexao->open();
