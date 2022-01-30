6a2cf8544cb45410409850cc8c5693166e261f6e
Erik Dias <erikdias@yahoo.com.br>
2022-01-29 23:10:11 -0300

    novo commit

---
+++ conexao.class.php
@@ -0,0 +1,37 @@
+<?php
+
+Class Conexao {
+    private $host = "ec2-3-214-190-189.compute-1.amazonaws.com";
+    private $user = "qcxueuwutugaom";
+    private $pswd = "534dfd6c06233dde14633d795ed6702154f227a41bfd773af7106c3466e3cc5f";
+    //private $banco = "dd8pp99pbdat4j";
+    private $banco;
+    private $strCon;
+    private $con;
+
+    function Conexao($banco) {
+        $this->banco = $banco;
+        $this->strCon = "host=$this->host user=$this->user password=$this->pswd dbname=$this->banco";
+    }
+    pg_connect("host=$this->host user=$this->user password=$this->pswd dbname=$this->banco");
+
+    function Open() {
+        $this->con=@pg_connect($this->strCon);
+    }
+
+    function Close() {
+        pg_close($this->con);
+    }
+
+    function StatusCon() {
+        if ($this->con) {
+            echo 'conectado';
+        }
+        else {
+            echo 'desconectado';
+            exit;
+        }
+    }
+}  
+
+?>
--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá Mundo</title>
-</head>
-<body>
-    <h1>
-        <?php
-        echo "Olá mundo!";
-        ?>
-    </h1>
-</body>
-</html>
+<?
+#incluindo a classe conexao (supondo estar no mesmo diretorio)
+include 'conexao.class.php'
+
+#instanciando o objeto
+$conexao = new Conexao('dd8pp99pbdat4j');
+
+#chamada ao metodo open que abra a conexao
+$conexao->open();
+
+#verificando o status da conexao
+$conexao->statusCon();
+
+#encerrando a conexao
+$conexao->close();
+
+#verificando a conexao apos close();
+$conexao->statusCon();
+?>
