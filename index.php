275a821ba4ab558bb71139cdc03b71e951fb4670
Erik Dias <erikdias@yahoo.com.br>
2022-02-22 02:15:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?
-#incluindo a classe conexao (supondo estar no mesmo diretorio)
-include 'conexao.class.php'
-
-#instanciando o objeto
-$conexao = new Conexao('dd8pp99pbdat4j');
-
-#chamada ao metodo open que abra a conexao
-$conexao->open();
-
-#verificando o status da conexao
-$conexao->statusCon();
-
-#encerrando a conexao
-$conexao->close();
-
-#verificando a conexao apos close();
-$conexao->statusCon();
+echo "PostgreSQL running..."
 ?>
